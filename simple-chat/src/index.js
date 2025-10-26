14085c632b201ab8c3c09fc22c64a7c31ffc830e
SergioMartinov31 <marseg06@mail.ru>
2025-10-26 18:18:55 +0300

    Add home-work 3

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -1,1 +1,8 @@
 import './index.css';
+// Установи картинку после загрузки DOM
+document.addEventListener('DOMContentLoaded', function() {
+    const img = document.querySelector('img[alt="attach file"]');
+    if (img) {
+        img.src = attachmentIcon;
+    }
+});
